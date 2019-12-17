bfccd3be826ac424764a61b14084084f4ee9e6eb
Victor <victorferreirait@gmail.com>
2019-12-17 17:36:47 -0300

    Removing unnecessary files and adding tests

--- heart.py
+++ heart.py
@@ -6,15 +6,11 @@ from sklearn.ensemble import ExtraTreesClassifier
 import sys
 
 # CHANGE THIS TO CORRECT FOLDER NAME
-HEART_PATH = os.path.join ("datasets", "heart")
+HEART_PATH = os.path.join ("datasets")
 
 def remove_rows_with_missing_values(data):
     df = data.copy().apply (pd.to_numeric, errors='coerce')
     return df.dropna()
-
-def load_heart_data():
-    csv_path = os.path.join(HEART_PATH, "cleveland.csv")
-    return pd.read_csv(csv_path)
 
 def load_heart_disease_data(filenames):
     dataframes = []
@@ -68,7 +64,7 @@ def get_inputs_as_matrix(args, keys, default=None):
     
     return [matrix_row]
 
-
+# Loads data
 heart = load_heart_disease_data([
     "cleveland.csv",
     "switzerland.csv",
@@ -76,28 +72,32 @@ heart = load_heart_disease_data([
     "longbeach.csv"
 ])
 
+# Cleans dataframe
 heart = replace_with_value(heart, 'thal', 0)
 heart = replace_with_mean(heart, 'ca')
 heart = replace_with_mode(heart, 'fbs')
-
 heart = remove_rows_with_missing_values(heart)
 
 labels = heart['num']
 examples = heart.drop('num', axis=1)
 
+#Creates train and test data sets
 test_size = .20
 train_set, test_set, train_labels, test_labels = train_test_split(examples, labels, test_size=test_size, random_state=42)
 
+#Creates model
 model = ExtraTreesClassifier()
 model.fit(train_set, train_labels)
 
 if sys.argv[1] == 'test':
+    # Test and print results
     result = model.score(test_set, test_labels)
 
     print("Total number of examples:", len(heart))
     print('Test size: %d' %(test_size * 100))
     print("Accuracy: ", result)
 else:
+    # Predict result
     keys = ["age","sex","cp","trestbps","chol","fbs","restecg","thalach","exang","oldpeak","slope","ca","thal"]
     inputs = get_inputs_as_matrix(sys.argv, keys, 0)
     
