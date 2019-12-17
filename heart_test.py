f43817b68700c95e100e43c1e70c3b29f8af63c9
Victor <victorferreirait@gmail.com>
2019-12-17 17:38:07 -0300

    Fixing test_get_inputs

--- heart_test.py
+++ heart_test.py
@@ -5,10 +5,10 @@ class TestHeart(unittest.TestCase):
     def test_get_inputs(self):
         #Assert if inputs contains expected keys
         inputs = heart.get_inputs(["val1=1"], ["val1"])
-        self.assertDictEqual(inputs, {"val1": 1})
+        self.assertDictEqual(inputs, {"val1": '1'})
 
         #Assert if inputs ignores extra keys
         inputs = heart.get_inputs(["val1=1", "val2=2"], ["val1"])
-        self.assertDictEqual(inputs, {"val1": 1})
+        self.assertDictEqual(inputs, {"val1": '1'})
 
 #python3 -m unittest heart_test
