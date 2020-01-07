52e953f63edc42d2d8d013e135bc6dd6954ac807
Victor <victorferreirait@gmail.com>
2020-01-07 15:30:46 -0300

    Changing init.sh to start python server

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo chown ubuntu -R ./
-sudo chmod +x -R ./
-touch foo.txt
-touch foo2.txt
-# pip3 install -r requirements.txt
-# python3 server.py
+# sudo chown ubuntu -R ./
+# sudo chmod +x -R ./
+sudo apt install python3-pip
+pip3 install -r requirements.txt
+python3 server.py
