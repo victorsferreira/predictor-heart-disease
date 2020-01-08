b451c7a1efcb34ad0abcd52fc985fa74be125a1e
Victor <victorferreirait@gmail.com>
2020-01-07 21:29:08 -0300

    Fixing quotes

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo `Installing pip3`
+echo "Installing pip3"
 # sudo apt install python3-pip
-echo `Installing Python dependencies`
+echo "Installing Python dependencies"
 # pip3 install -r /home/ubuntu/predictor/requirements.txt
-echo `Will start server on port 5000`
+echo "Will start server on port 5000"
 python3 /home/ubuntu/predictor/server.py &
