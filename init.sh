6819d397112b6ad23583e3972c92f7889a4a77c0
Victor <victorferreirait@gmail.com>
2020-01-07 21:02:55 -0300

    Fixing absolute paths in init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 ls
 sudo apt install python3-pip
-pip3 install -r /home/ubuntu/requirements.txt
-python3 /home/ubuntu/server.py
+pip3 install -r /home/ubuntu/predictor/requirements.txt
+python3 /home/ubuntu/predictor/server.py
