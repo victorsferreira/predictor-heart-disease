4410f6ab0d6e24ff34c9810a055d08c9404269dc
Victor <victorferreirait@gmail.com>
2020-01-11 21:53:27 -0300

    Adding nohup to process with http-server

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -5,4 +5,4 @@ sudo apt install python3-pip
 echo "Installing Python dependencies"
 pip3 install -r /home/ubuntu/predictor/requirements.txt
 echo "Server 5000"
-http-server /home/ubuntu/predictor -p 5000
+nohup http-server /home/ubuntu/predictor -p 5000 </dev/null &>/dev/null &
